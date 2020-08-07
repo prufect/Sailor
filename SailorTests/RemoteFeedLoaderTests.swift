ae75ee8444d35cc1698d7fa9c4779f4d4827e801
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-07 09:28:07 -0700

    Test Remote Feed Loader does not request URL on Init

---
+++ SailorTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,25 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  SailorTests
+//
+//  Created by Prudhvi Gadiraju on 8/7/20.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+
+        XCTAssertNil(client.requestedURL)
+    }
+}
