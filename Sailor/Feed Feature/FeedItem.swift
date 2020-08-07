bd2d0e83e58e9d4135af4f11065fb6c5d3c90888
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-07 09:17:29 -0700

    Setup Feed Feature

---
+++ Sailor/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Sailor
+//
+//  Created by Prudhvi Gadiraju on 8/7/20.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
---
+++ Sailor/Feed Feature/FeedLoader.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLoader.swift
+//  Sailor
+//
+//  Created by Prudhvi Gadiraju on 8/7/20.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
