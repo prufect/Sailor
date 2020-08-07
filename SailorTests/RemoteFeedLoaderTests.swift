c97aabf2f0b00d7edb805eda49f785690ce4f127
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-07 11:22:48 -0700

    Improve Error Test to be more realistic and async

--- SailorTests/RemoteFeedLoaderTests.swift
+++ SailorTests/RemoteFeedLoaderTests.swift
@@ -26,10 +26,12 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 1)
         
         var capturedErrors: [RemoteFeedLoader.Error] = []
+        let error = NSError(domain: "Test", code: 1)
+
         sut.load { capturedErrors.append($0) }
+        client.complete(with: error)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
@@ -47,14 +49,15 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs: [URL] = []
-        var error: Error?
+        var completions: [(Error?) -> Void] = []
         
         func get(from url: URL, completion: @escaping (Error?) -> Void) {
-            if let error = error {
-                completion(error)
-            }
-            
+            completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
         
     }
