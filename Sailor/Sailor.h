21ff560eecfde923bc5edfae87995c9ab16239ea
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-07 09:14:00 -0700

    Initial Commit

---
+++ Sailor/Sailor.h
@@ -0,0 +1,18 @@
+//
+//  Sailor.h
+//  Sailor
+//
+//  Created by Prudhvi Gadiraju on 8/7/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sailor.
+FOUNDATION_EXPORT double SailorVersionNumber;
+
+//! Project version string for Sailor.
+FOUNDATION_EXPORT const unsigned char SailorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sailor/PublicHeader.h>
+
+
