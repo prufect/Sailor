e7b796358c7ea1c576854da988ef6888d1c68e94
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-08 11:02:24 -0700

    Test Memory Leaks

--- SailorTests/RemoteFeedLoaderTests.swift
+++ SailorTests/RemoteFeedLoaderTests.swift
@@ -97,12 +97,23 @@ class RemoteFeedLoaderTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT(
-        url: URL = URL(string: "https://a-url.com")!)
+        url: URL = URL(string: "https://a-url.com")!,
+        file: StaticString = #filePath, line: UInt = #line)
             -> (sut: RemoteFeedLoader, client: HTTPClientSpy)
     {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        
         return (sut, client)
+    }
+    
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Potential Memory Leak", file: file, line: line)
+        }
     }
     
     private func makeItem(
