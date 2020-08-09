ccb36e864f5a187f9dd09301d8191eb24ddebe32
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-09 12:13:02 -0700

    Move interception to setup and tear down methods

--- SailorTests/Feed API/URLSessionHTTPClientTests.swift
+++ SailorTests/Feed API/URLSessionHTTPClientTests.swift
@@ -26,8 +26,17 @@ class URLSessionHTTPClient {
 
 class URLSessionHTTPClientTests: XCTestCase {
     
-    func test_getFromURL_performsGETRequestWithURL() {
+    override class func setUp() {
+        super.setUp()
         URLProtocolStub.startInterceptingRequests()
+    }
+    
+    override class func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
+    func test_getFromURL_performsGETRequestWithURL() {
         let url = URL(string: "https://any-url.com")!
 
         let exp = expectation(description: "Wait for Request")
@@ -42,12 +51,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         sut.get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     func test_getFromURL_failsOnRequestError() {
-        URLProtocolStub.startInterceptingRequests()
-        
         let url = URL(string: "https://any-url.com")!
         let error = NSError(domain: "Test Error", code: 1)
         URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
@@ -67,8 +73,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
-        
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     private class URLProtocolStub: URLProtocol {
