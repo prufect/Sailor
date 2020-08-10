ce2a49dc4e0a361837c8acc24cf156bafa61d655
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-10 10:16:35 -0700

    Move `FeedStore` to its own file

---
+++ Sailor/Feed Cache/FeedStore.swift
@@ -0,0 +1,17 @@
+//
+//  FeedStore.swift
+//  Sailor
+//
+//  Created by Prudhvi Gadiraju on 8/10/20.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
+
