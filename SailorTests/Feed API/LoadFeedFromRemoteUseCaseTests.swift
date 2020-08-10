855d5f2c37ee5d4ca269c73006184fa5ff5c747a
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-10 00:44:34 -0700

    Rename RemoteFeedLoaderTests to LoadFeedFromRemoteUseCaseTests to reflect the use case it refers to

--- SailorTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ SailorTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -1,5 +1,5 @@
 //
-//  RemoteFeedLoaderTests.swift
+//  LoadFeedFromRemoteUseCaseTests.swift
 //  SailorTests
 //
 //  Created by Prudhvi Gadiraju on 8/7/20.
@@ -8,7 +8,7 @@
 import XCTest
 import Sailor
 
-class RemoteFeedLoaderTests: XCTestCase {
+class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
         
