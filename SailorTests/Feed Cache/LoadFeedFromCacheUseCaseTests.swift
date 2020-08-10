cc74d2fa6b6e7fad5daac79772c62525e2073aaa
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-10 12:09:43 -0700

    Load method delivers no images on empty cache

--- SailorTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ SailorTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -46,6 +46,28 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         
         XCTAssertEqual(retrievalError, receivedError as NSError?)
     }
+    
+    func test_load_deliversNoImagesOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        let exp = expectation(description: "Wait for Completion")
+
+        var receivedImages: [FeedImage] = []
+        sut.load { result in
+            switch result {
+            case let .success(images):
+                receivedImages = images
+            default:
+                XCTFail("Expected success, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+
+        store.completeRetrievalWithEmptyCache()
+        wait(for: [exp], timeout: 1.0)
+
+        XCTAssertEqual(receivedImages, [])
+    }
 
     // MARK: - Helpers
     
