e4c293c6ab22762405e40c9da6e43563906f3d49
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-09 21:03:08 -0700

    Add Memory Leak Tracking

--- SailorAPIEndToEndTests/SailorAPIEndToEndTests.swift
+++ SailorAPIEndToEndTests/SailorAPIEndToEndTests.swift
@@ -26,11 +26,14 @@ class SailorAPIEndToEndTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func getFeedResult() -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
         
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+
         let exp = expectation(description: "Wait for load completion")
         
         var receivedResult: LoadFeedResult?
