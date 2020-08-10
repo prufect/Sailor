df5002fb936afb367da1a8505075f0e9ebf3809a
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-10 09:29:25 -0700

    Save method successfully saves on successful insertion

--- SailorTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ SailorTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -63,6 +63,10 @@ class FeedStore {
     func completeInsertion(with error: Error, at index: Int = 0) {
         insertionCompletions[index](error)
     }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -141,6 +145,25 @@ class CacheFeedUseCaseTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
         XCTAssertEqual(insertionError, receivedError)
+    }
+    
+    func test_save_succeedsOnSuccessfulCacheInsertion() {
+        let timestamp = Date()
+        let items = [uniqueItem(), uniqueItem()]
+        let (sut, store) = makeSUT(timestamp: { timestamp })
+        
+        let exp = expectation(description: "Wait for Completion")
+        var receivedError: NSError?
+        sut.save(items) { error in
+            receivedError = error as NSError?
+            exp.fulfill()
+        }
+        
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertNil(receivedError)
     }
     
     // MARK: - Helpers
