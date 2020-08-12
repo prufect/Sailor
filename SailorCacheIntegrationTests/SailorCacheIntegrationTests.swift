5ff7eb06c440f4dfdeca7f162b20b29449a4ed4b
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-08-12 10:20:23 -0700

    Test load from different instances

--- SailorCacheIntegrationTests/SailorCacheIntegrationTests.swift
+++ SailorCacheIntegrationTests/SailorCacheIntegrationTests.swift
@@ -10,6 +10,16 @@ import Sailor
 
 class SailorCacheIntegrationTests: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
         
@@ -28,6 +38,36 @@ class SailorCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_load_deliversItemsSavedOnSeparateInstance() {
+        let sutToPerformSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let feed = uniqueImageFeed().models
+        
+        let saveExp = expectation(description: "Wait for Save")
+        sutToPerformSave.save(feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save successfully")
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 1.0)
+        
+        let loadExp = expectation(description: "Wait for Load")
+        sutToPerformLoad.load { loadResult in
+            switch loadResult {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, feed)
+            
+            case let .failure(error):
+                XCTFail("Expected success got \(error) instead")
+            }
+            
+            loadExp.fulfill()
+        }
+        
+        wait(for: [loadExp], timeout: 1.0)
+    }
+    
+    // MARK: - Helpers
+    
     func makeSUT() -> LocalFeedLoader {
         let store = CodableFeedStore(storeURL: testStoreURL())
         let sut = LocalFeedLoader(store: store, currentDate: Date.init)
@@ -38,5 +78,17 @@ class SailorCacheIntegrationTests: XCTestCase {
     
     func testStoreURL() -> URL {
         FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testStoreURL())
     }
 }
